dd71fc2d23e25f9c2a6d7d13ec7dea50b1bf0385
TedKuo <t1162625@gmail.com>
2021-06-29 20:48:20 +0800

    feat: 新增路由表

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> -->
+  <!-- <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+
+  <button type="button" class="btn btn-link">Link</button> -->
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import './assets/all';
 
-#nav {
-  padding: 30px;
+// OOCSS -> 複用性高
+// BEM -> 便於開發、但程式碼會比較長
+// 寫在這比較像 BEM 概念
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// 如果全域會使用的套件，就在 main.js 引入
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
