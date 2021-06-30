702d02f9cc5c9f25142aab459478c47c59caaa63
TedKuo <t1162625@gmail.com>
2021-06-30 21:12:20 +0800

    feat: 更新路由表

--- src/router/index.js
+++ src/router/index.js
@@ -7,16 +7,32 @@ const routes = [
     component: () => import('../views/Front.vue'),
     children: [
       {
+        path: '',
+        component: () => import('../views/Index.vue')
+      },
+      {
         path: 'products',
         component: () => import('../views/Products.vue')
       },
       {
-        path: '/about',
+        path: 'product/:id',
+        component: () => import('../views/Product.vue')
+      },
+      {
+        path: 'about',
         name: 'About',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      },
+      {
+        path: 'login',
+        component: () => import('../views/Login.vue')
+      },
+      {
+        path: 'cart',
+        component: () => import('../views/Cart.vue')
       }
     ]
   },
--- src/views/Front.vue
+++ src/views/Front.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">導覽列</a>
+  <div class="container">
+    <a class="navbar-brand" href="#">前台導覽列</a>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
@@ -10,6 +10,9 @@
         </li>
         <li class="nav-item">
           <router-link class="nav-link" to="/products">產品列表</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/cart">前台購物車</router-link>
         </li>
       </ul>
     </div>
